dfc829af688d481d89f188bb76a517b86cdc3990
SebasUr <sebasurberu1@gmail.com>
2023-05-25 00:53:51 -0500

    a

--- CsvR.java
+++ CsvR.java
@@ -86,12 +86,35 @@ public class CsvR {
 
     public void imprimir(){
         for(int i =0; i<data.length; i++){
-            for(int j=0; j<data[i].length-23; j++) // el length de la fila actual.
+            for(int j=0; j<data[i].length-18; j++) // el length de la fila actual.
             {
                 System.out.print(data[i][j] + "\t");
             }
             System.out.println();
         }
+    }
+
+    public String[][] combinarMatrices(String[][] matriz1, String[][] matriz2) {
+        int filas1 = matriz1.length;
+        int columnas1 = matriz1[0].length;
+        int filas2 = matriz2.length;
+        int columnas2 = matriz2[0].length;
+
+        String[][] matrizCombinada = new String[filas1 + filas2][columnas1];
+
+        for (int i = 0; i < filas1; i++) {
+            for (int j = 0; j < columnas1; j++) {
+                matrizCombinada[i][j] = matriz1[i][j];
+            }
+        }
+
+        for (int i = 0; i < filas2; i++) {
+            for (int j = 0; j < columnas2; j++) {
+                matrizCombinada[filas1 + i][j] = matriz2[i][j];
+            }
+        }
+
+        return matrizCombinada;
     }
 
     public String contarNulosYVacios(int columna, String[][] matriz) {
